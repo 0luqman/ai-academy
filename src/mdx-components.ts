d9cf19fe5753353618ff2a173b4d220448523fb5
Error REd <mirmohmmadluqman@gmail.com>
2026-03-01 05:30:39 -0800

    Vercel Deploy Err fix #atempt2

--- src/mdx-components.ts
+++ src/mdx-components.ts
@@ -1,11 +1,9 @@
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
-import type { UseMDXComponents } from 'nextra/mdx-components'
 import { mdxComponents as customComponents } from '@/components/mdx-components'
 
-const themeComponents = getThemeComponents()
-
-export const useMDXComponents: UseMDXComponents<typeof themeComponents> = <T>(components: T) => ({
-    ...themeComponents,
-    ...customComponents,
-    ...components
-})
+export function useMDXComponents(components: any): any {
+    return {
+        ...getThemeComponents(components),
+        ...customComponents
+    }
+}
